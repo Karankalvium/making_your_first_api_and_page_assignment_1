f663512aa21fe49aa70e6ccf9800199d584e2d35
Karan-1208 <mkarandeep.1208@gmail.com>
2024-12-04 15:12:36 +0530

    completed the project

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // Boilerplate Code for Virtual Assistant API
 const express = require('express');
+const { request } = require('http');
 const app = express();
 
 /*
@@ -32,6 +33,38 @@ Example Responses:
 
 Add the required logic below to complete the API.
 */
+app.get("/assistant/greet",(request,response)=>{
+  let name = request.query.name;
+  let day = new Date().getDay();
+  if(day==1)
+  {
+    let obj={
+      welcomeMessage: `Hello, ${name}! Welcome to our assistant app!`,
+      dayMessage: 'Happy Monday! Start your week with energy!',
+    };
+    return response.send(obj);
+  }
+  else if (day == 5)
+  {
+    let obj={
+      welcomeMessage: `Hello, ${name}! Welcome to our assistant app!`,
+      dayMessage: "It's Friday! The weekend is near!",
+    };
+    return response.send(obj);
+  }
+  else
+  {
+    let obj={
+      welcomeMessage: `Hello, ${name}! Welcome to our assistant app!`,
+      dayMessage: 'Have a wonderful day!',
+    };
+    return response.send(obj);
+  }
+});
+
+app.get('/', function(request,response){
+  return response.send('<h1>Hello World</h1>');
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
